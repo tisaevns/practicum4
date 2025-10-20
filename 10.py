fd3b447ac2a889cb238240e2e3206d5a30deff4d
tisaevns <t.isaev@g.nsu.ru>
2025-10-20 22:20:51 +0700

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+if n < 2:
+    print('нет')
+elif n % 2 == 0:
+    print('да')
+else:
+    print('нет')
---
+++ 5.py
@@ -0,0 +1,6 @@
+N = int(input())
+K = int(input())
+if N < K:
+    print(N)
+else:
+    print(K)
